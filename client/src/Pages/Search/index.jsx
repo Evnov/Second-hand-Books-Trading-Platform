8c63bacb89f9b956e819bf1f69ada4e1f3bd4310
Evnov <eviipan1996@gmail.com>
2020-11-06 17:18:50 -0800

    search module update

--- client/src/Pages/Search/index.jsx
+++ client/src/Pages/Search/index.jsx
@@ -5,21 +5,46 @@ class SearchResult extends Component {
   render() {
     return (
       <div className={styles.search}>
-        <h3>
-          {this.props.location.items.length} searching results for "
-          {this.props.location.query}":
-        </h3>
-        <div className={styles.books}>
-          {this.props.location.items.map((item, index) => {
-            let { title, imageLink, infoLink } = item.volumeInfo;
-            return (
-              <div className={styles.bookDiv} key={index}>
-                <header className={styles.bookTitle}>{title}</header>
-                <footer className={styles.bookIndex}>Price: ${index + 1}</footer>
-              </div>
-            );
-          })}
-        </div>
+        {this.props.location.items.length === 0 ? (
+          <h3>0 searching results for "{this.props.location.query}":</h3>
+        ) : (
+          <div>
+            <h3>
+              {this.props.location.items.length} searching results for "
+              {this.props.location.query}":
+            </h3>
+            <div className={styles.books}>
+              {this.props.location.items.map((item, index) => {
+                let { title, authors, publishedDate } = item.volumeInfo;
+                return (
+                  <div className={styles.bookDiv} key={index}>
+                    <div className={styles.bookImg}></div>
+                    <div className={styles.bookInfo}>
+                      <header className={styles.bookTitle}>{title}</header>
+                      <section className={styles.bookAuthor}>
+                        {authors[0] === null ? "" : authors[0]}
+                      </section>
+                      <section className={styles.bookPrice}>
+                        ${Math.floor(Math.random() * (1000 - 100) + 100) / 100}
+                      </section>
+                      <section className={styles.bookSection}>
+                        Published: {publishedDate}
+                      </section>
+                      <section className={styles.bookSection}>
+                        Department: ICS
+                      </section>
+                      <section>Course: 260P Application of Algorithm </section>
+                    </div>
+                    <div className={styles.bookBtn}>
+                      <button className={styles.btn}>Add to Cart</button>
+                      <button className={styles.btn}>Add to WatchList</button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
