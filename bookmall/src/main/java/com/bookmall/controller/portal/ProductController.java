bc1dac75ea1f32597595f273e8b987ced197055d
ellcyyang <ellcy1994@gmail.com>
2020-11-27 23:13:16 -0800

    add cors

--- bookmall/src/main/java/com/bookmall/controller/portal/ProductController.java
+++ bookmall/src/main/java/com/bookmall/controller/portal/ProductController.java
@@ -9,13 +9,12 @@ import com.bookmall.service.IBookService;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import javax.servlet.http.HttpSession;
 
+@CrossOrigin()
 @Controller
 @RequestMapping("/product/")
 public class ProductController {
